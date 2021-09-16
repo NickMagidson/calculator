905b57d9b35ba04ecb2648faf95111726f71ffe5
Nick <nickmagi24@gmail.com>
2021-09-16 11:28:05 -0400

    Operate function

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ function divide(a, b) {
     return a / b;
 }
 
+// Operate function
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return subtract(a, b)
+        case '*':
+            return multiply(a, b)
+        case '/':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
+    }
+}
+
+
+
+
 
 console.log(add(2, 2)) // 4
 console.log(subtract(5, 3)) // 2
