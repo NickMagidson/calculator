5d92350e668ecd1d7a1e03e56b96e513b530f1cc
Nick <nickmagi24@gmail.com>
2021-09-16 11:22:22 -0400

    Console tests

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 //Basic operator functions
 function add(a, b) {
-    
+    return a + b;
 }
 
 function subtract(a, b) {
-    
+    return a - b;
 }
 
 function multiply(a, b) {
-    
+    return a * b;
 }
 
 function divide(a, b) {
-    
+    return a / b;
 }
+
+
+console.log(add(2, 2)) // 4
+console.log(subtract(5, 3)) // 2
+console.log(multiply(2, 5)) // 10
+console.log(divide(6, 3)) // 2
